f2359a7377ab371a1171ee1b63237c0bc95018d4
Omar Abdel-Wahab <owahab@gmail.com>
2016-09-27 03:13:45 +0200

    Adds Gub forumla

---
+++ gogub.rb
@@ -0,0 +1,17 @@
+class Gogub < Formula
+  desc "The missing Github command-line"
+  homepage "https://github.com/owahab/gogub"
+  url "https://github.com/owahab/gogub.git",
+    tag: "0.0.3"
+
+  depends_on "go" => :build
+
+  def install
+    system "gobuild.sh"
+    bin.install ".gobuild/bin/gub" => "gub"
+  end
+
+  test do
+    system "#{bin}/gub", "--help"
+  end
+end
